c8a65fa8a101a4fe561167af1d20b8d5aa0e2bae
Bruno Bord <bruno@la-chose>
2008-02-16 18:45:48 +0100

    Massive merge from Adam Rooke's branch (code-reorganise). Now the main pyroom file is splitted in parts, hence it'll be easier to contribute and implement new features. Big thanks to everyone, I think it's a milestone in this project.

---
+++ status_label.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#Includes the class for status label. 
+
+import gobject
+import gtk
+
+class FadeLabel(gtk.Label):
+    """ GTK Label with timed fade out effect """
+
+    active_duration = 3000  # Fade start after this time
+    fade_duration = 1500.0  # Fade duration
+
+    def __init__(self, message='', active_color=None, inactive_color=None):
+        gtk.Label.__init__(self, message)
+        if not active_color:
+            active_color = '#ffffff'
+        self.active_color = active_color
+        if not inactive_color:
+            inactive_color = '#000000'
+        self.inactive_color = inactive_color
+        self.idle = 0
+
+    def set_text(self, message, duration=None):
+        if not duration:
+            duration = self.active_duration
+        self.modify_fg(gtk.STATE_NORMAL,
+                       gtk.gdk.color_parse(self.active_color))
+        gtk.Label.set_text(self, message)
+        if self.idle:
+            gobject.source_remove(self.idle)
+        self.idle = gobject.timeout_add(duration, self.fade_start)
+
+    def fade_start(self):
+        self.fade_level = 1.0
+        if self.idle:
+            gobject.source_remove(self.idle)
+        self.idle = gobject.timeout_add(25, self.fade_out)
+
+    def fade_out(self):
+        color = gtk.gdk.color_parse(self.inactive_color)
+        (red1, green1, blue1) = (color.red, color.green, color.blue)
+        color = gtk.gdk.color_parse(self.active_color)
+        (red2, green2, blue2) = (color.red, color.green, color.blue)
+        red = red1 + int(self.fade_level * abs(red1 - red2))
+        green = green1 + int(self.fade_level * abs(green1 - green2))
+        blue = blue1 + int(self.fade_level * abs(blue1 - blue2))
+        self.modify_fg(gtk.STATE_NORMAL, gtk.gdk.Color(red, green, blue))
+        self.fade_level -= 1.0 / (self.fade_duration / 25)
+        if self.fade_level > 0:
+            return True
+        self.idle = 0
+        return False
---
+++ styles.py
@@ -0,0 +1,90 @@
+styles = {
+
+    'darkgreen': {
+    'name': 'darkgreen',
+    'background': '#000000',
+    'foreground': '#007700',
+    'lines': '#001100',
+    'border': '#001100',
+    'info': '#007700',
+    'font': 'Droid Sans Mono',
+    'fontsize': 12,
+    'padding': 6,
+    'size': [0.6, 0.95],
+    }, 
+     
+     'green': {
+    'name': 'green',
+    'background': '#000000',
+    'foreground': '#00ff00',
+    'lines': '#007700',
+    'border': '#003300',
+    'info': '#00ff00',
+    'font': 'Droid Sans Mono',
+    'fontsize': 12,
+    'padding': 6,
+    'size': [0.6, 0.95],
+    }, 
+    
+    'blue': {
+    'name': 'blue',
+    'background': '#0000ff',
+    'foreground': '#ffffff',
+    'lines': '#5555ff',
+    'border': '#3333ff',
+    'info': '#ffffff',
+    'font': 'Droid Sans Mono',
+    'fontsize': 12,
+    'padding': 6,
+    'size': [0.6, 0.95],
+    },
+    'c64': {
+    'name' : 'c64',
+    'background' : '#10009C',
+    'foreground' : '#9C9CFF',
+    'lines' : '#3B3BBD',
+    'border' : '#10009C',
+    'info' : '#7373FF',
+    'font' : 'DejaVu Sans Mono',
+    'fontsize' : 12,
+    'padding' : 8,
+    'size' : [0.45, 0.75] # [width, height]
+    },
+    'locontrast' : {
+        'name' : 'locontrast',
+        'background' : '#6F6F6F',
+        'foreground' : '#212121',
+        'lines' : '#2F455B',
+        'border' : '#6F6F6F',
+        'info' : '#C44700',
+        'font' : 'DejaVu Sans Mono',
+        'fontsize' : 9,
+        'padding' : 8,
+        'size' : [0.45, 0.75] # [width, height]
+        },
+    'cupid' : {
+        'name' : 'cupid',
+        'background' : '#12000B',
+        'foreground' : '#D68C8B',
+        'lines' : '#733B44',
+        'border' : '#12000B',
+        'info' : '#DEB8A6',
+        'font' : 'DejaVu Sans Mono',
+        'fontsize' : 9,
+        'padding' : 8,
+        'size' : [0.45, 0.75] # [width, height]
+        },
+        
+    'banker' : {
+        'name' : 'banker',
+        'background' : '#111D00',
+        'foreground' : '#DED291',
+        'lines' : '#7D9635',
+        'border' : '#111D00',
+        'info' : '#7D9635',
+        'font' : 'DejaVu Sans Mono',
+        'fontsize' : 9,
+        'padding' : 8,
+        'size' : [0.45, 0.75] # [width, height]
+        }
+    }
