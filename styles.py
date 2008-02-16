114907c13aba76947e50b59637d617bcd7f513af
Bruno Bord <bruno@la-chose>
2008-02-16 19:05:48 +0100

    * Styles tiny cleanup. * Amber style added - closes #189261 * Ignoring "session" file

--- styles.py
+++ styles.py
@@ -1,55 +1,56 @@
 styles = {
-
     'darkgreen': {
-    'name': 'darkgreen',
-    'background': '#000000',
-    'foreground': '#007700',
-    'lines': '#001100',
-    'border': '#001100',
-    'info': '#007700',
-    'font': 'Droid Sans Mono',
-    'fontsize': 12,
-    'padding': 6,
-    'size': [0.6, 0.95],
-    }, 
-     
+        'name': 'darkgreen',
+        'background': '#000000',
+        'foreground': '#007700',
+        'lines': '#001100',
+        'border': '#001100',
+        'info': '#007700',
+        'font': 'Droid Sans Mono',
+        'fontsize': 12,
+        'padding': 6,
+        'size': [0.6, 0.95],
+    },
+
      'green': {
-    'name': 'green',
-    'background': '#000000',
-    'foreground': '#00ff00',
-    'lines': '#007700',
-    'border': '#003300',
-    'info': '#00ff00',
-    'font': 'Droid Sans Mono',
-    'fontsize': 12,
-    'padding': 6,
-    'size': [0.6, 0.95],
-    }, 
-    
+        'name': 'green',
+        'background': '#000000',
+        'foreground': '#00ff00',
+        'lines': '#007700',
+        'border': '#003300',
+        'info': '#00ff00',
+        'font': 'Droid Sans Mono',
+        'fontsize': 12,
+        'padding': 6,
+        'size': [0.6, 0.95],
+    },
+
     'blue': {
-    'name': 'blue',
-    'background': '#0000ff',
-    'foreground': '#ffffff',
-    'lines': '#5555ff',
-    'border': '#3333ff',
-    'info': '#ffffff',
-    'font': 'Droid Sans Mono',
-    'fontsize': 12,
-    'padding': 6,
-    'size': [0.6, 0.95],
+        'name': 'blue',
+        'background': '#0000ff',
+        'foreground': '#ffffff',
+        'lines': '#5555ff',
+        'border': '#3333ff',
+        'info': '#ffffff',
+        'font': 'Droid Sans Mono',
+        'fontsize': 12,
+        'padding': 6,
+        'size': [0.6, 0.95],
     },
+
     'c64': {
-    'name' : 'c64',
-    'background' : '#10009C',
-    'foreground' : '#9C9CFF',
-    'lines' : '#3B3BBD',
-    'border' : '#10009C',
-    'info' : '#7373FF',
-    'font' : 'DejaVu Sans Mono',
-    'fontsize' : 12,
-    'padding' : 8,
-    'size' : [0.45, 0.75] # [width, height]
+        'name' : 'c64',
+        'background' : '#10009C',
+        'foreground' : '#9C9CFF',
+        'lines' : '#3B3BBD',
+        'border' : '#10009C',
+        'info' : '#7373FF',
+        'font' : 'DejaVu Sans Mono',
+        'fontsize' : 12,
+        'padding' : 8,
+        'size' : [0.45, 0.75] # [width, height]
     },
+
     'locontrast' : {
         'name' : 'locontrast',
         'background' : '#6F6F6F',
@@ -62,6 +63,7 @@ styles = {
         'padding' : 8,
         'size' : [0.45, 0.75] # [width, height]
         },
+
     'cupid' : {
         'name' : 'cupid',
         'background' : '#12000B',
@@ -74,7 +76,7 @@ styles = {
         'padding' : 8,
         'size' : [0.45, 0.75] # [width, height]
         },
-        
+
     'banker' : {
         'name' : 'banker',
         'background' : '#111D00',
@@ -86,5 +88,18 @@ styles = {
         'fontsize' : 9,
         'padding' : 8,
         'size' : [0.45, 0.75] # [width, height]
-        }
+        },
+
+    'amber' : {
+        'name' : 'amber',
+        'background' : '#151000',
+        'foreground' : '#AE8400',
+        'lines' : '#AE8400',
+        'border' : '#151000',
+        'info' : '#AE8400',
+        'font' : 'DejaVu Sans Mono',
+        "fontsize": 9,
+        'padding' : 8,
+        'size' : [0.45, 0.75] # [width, height]
+        },
     }
