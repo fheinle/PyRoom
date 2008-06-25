b732177d9f05f203ceaa62959d69dd83fefe90db
Adam Rooke <adam@jesse>
2008-06-25 23:48:51 +0000

    Added license again as well as docstring

--- basic_edit.py
+++ basic_edit.py
@@ -1,14 +1,29 @@
+# basic_edit.py - provides editor functionality
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# PyRoom - A clone of WriteRoom
+# Copyright (c) 2007 Nicolas P. Rougier & NoWhereMan
+# Copyright (c) 2008 The Pyroom Team - See AUTHORS file for more information
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
 
-"""
-    PyRoom - A clone of WriteRoom
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""editor.py - provides the editing logic
 
-    Editing logic
-
-    :copyright: 2007 Nicolas P. Rougier & NoWhereMan Copyright
-    :copyright: 2008 The PyRoom Theme - See AUTHORS file for more information
-    :license: GNU General Public License, version 3 or later
+contains basic functions needed for pyroom - any core functionality is included
+within this file
+    
 """
 
 import gtk
