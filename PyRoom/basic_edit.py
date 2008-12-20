91b810287eae0e4b091bb915508be16f4d9cbf10
Florian Heinle <florian-launchpad@planet-tiax.de>
2008-12-21 00:14:31 +0100

    create nonexistant files when asked to from command line, fixes LP:#307257

--- PyRoom/basic_edit.py
+++ PyRoom/basic_edit.py
@@ -280,20 +280,20 @@ open the file.')
             buf.set_text(utf8)
             buf.end_not_undoable_action()
             buffer_file.close()
-            self.status.set_text(_('File %s open')
-                     % buf.filename)
         except IOError, (errno, strerror):
             errortext = _('Unable to open %(filename)s.' % {
                 'filename': buf.filename})
-            if errno == 2:
-                errortext += _(' The file does not exist.')
-            elif errno == 13:
+            if errno == 13:
                 errortext += _(' You do not have permission to open \
 the file.')
-            raise PyroomError(errortext)
+            if not errno == 2:
+                raise PyroomError(errortext)
         except:
-            raise PyroomError(_('Unable to open %s\n'
-                             % buf.filename))
+            raise
+            raise PyroomError(_('Unable to open %s\n' % buf.filename))
+        else:
+            self.status.set_text(_('File %s open') % buf.filename)
+
     def save_file(self):
         """ Save file """
         try:
