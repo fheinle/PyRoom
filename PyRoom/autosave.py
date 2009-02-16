742d97773a3718fd1c6cdbf102e11c7d8d1496d7
Florian Heinle <florian-launchpad@planet-tiax.de>
2009-02-16 18:26:04 +0100

    fixes

--- PyRoom/autosave.py
+++ PyRoom/autosave.py
@@ -26,38 +26,41 @@ import gobject
 from pyroom_error import PyroomError
 import os
 
-def start_autosave(autosave_time, edit_instance):
+def start_autosave(edit_instance):
     """start the autosave timer"""
-    timeout_id = gobject.timeout_add(60000, autosave_timeout, edit_instance)
+    timeout_id = gobject.timeout_add(1000, autosave_timeout, edit_instance)
     edit_instance.autosave_timeout_id = timeout_id
     edit_instance.autosave_elapsed = 0
 
 def stop_autosave(edit_instance):
     """stop the autosave timer and remove backup files"""
     for buf in edit_instance.buffers:
-        if not buf.filename = edit_instance.UNNAME_FILENAME and \
+        if not buf.filename == edit_instance.UNNAMED_FILENAME and \
            os.path.isfile(buf.filename + "~"):
-            os.remove buf.filename
+            os.remove(buf.filename)
     gobject.source_remove(edit_instance.autosave_timeout_id)
 
 def autosave_timeout(edit_instance):
     """see if we have to autosave open files"""
-    if edit_instance.autosave_elapsed >= edit_instance.autosave_time:
-        autosave(edit_instance)
-        edit_instance.autosave_elapsed = 0
-    else:
-        edit_instance.autosave_elapsed += 1
+    if edit_instance.preferences.autosave_time:
+        if edit_instance.autosave_elapsed >= \
+           edit_instance.preferences.autosave_time * 60:
+            autosave(edit_instance)
+            edit_instance.autosave_elapsed = 0
+        else:
+            edit_instance.autosave_elapsed += 1
     return True
 
 def autosave(edit_instance):
     """save all open files that have been saved before"""
     for buf in edit_instance.buffers:
-        if not buf.filename = edit_instance.UNNAMED_FILENAME:
-            backup_file = open(edit_instance.filename + '~', 'w')
+        if not buf.filename == edit_instance.UNNAMED_FILENAME:
+            backup_file = open(buf.filename + '~', 'w')
             try:
                 backup_file.write(
                     buf.get_text(buf.get_start_iter(), buf.get_end_iter())
                 )
             except IOError:
                 raise PyroomError(_("Could not autosave file %s") % filename)
-            edit_instance.status.set_text(_('AutoSaving Buffer %(buf_id)d'))
+            finally:
+                backup_file.close()
